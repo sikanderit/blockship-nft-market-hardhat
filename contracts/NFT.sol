de2df5bae82373a8df160a13d85b0ae146c7e5e0
Sikander Ali <71672759+sikandermit@users.noreply.github.com>
2021-10-28 20:32:50 +0500

    Update NFT.sol

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.3;
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-
+import "hardhat/console.sol";
 
 
 contract NFT is ERC721URIStorage {
