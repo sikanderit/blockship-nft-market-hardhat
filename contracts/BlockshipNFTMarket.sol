e16bf127a052277d2828b870a99c269a3059a686
Sikander Ali <71672759+sikandermit@users.noreply.github.com>
2021-10-28 20:33:13 +0500

    Update BlockshipNFTMarket.sol

--- contracts/BlockshipNFTMarket.sol
+++ contracts/BlockshipNFTMarket.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.3;
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "hardhat/console.sol";
 
 contract BlockshipNFTMarket is ReentrancyGuard {
   using Counters for Counters.Counter;
